4ee4ddfa38e40494ad40ff8d74004f1c4c2df365
wshine <114693487+WinstonShine@users.noreply.github.com>
2024-04-18 05:29:28 +0000

    update

--- flake.nix
+++ flake.nix
@@ -1,35 +1,22 @@
 {
-  description = "Nixos config flake";
-
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      homeConfiguration."crowll" =
-        inputs.home-manager.lib.homeManagerConfiguration {
-	  inherit pkgs;
-	  modules = [./home.nix ];
-	  extraSpecialArgs = { inherit inputs; };
-	};
-      nixosConfigurations = {
-        default = nixpkgs.lib.nixosSystem {
-            specialArgs = {inherit inputs;};
-            modules = [
-	    	./nixos/configuration.nix
-		inputs.home-manager.nixosModules.default
-            ];
-        };
+  outputs = { self, nixpkgs, nixos-wsl, ... }: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          nixos-wsl.nixosModules.default
+          {
+            system.stateVersion = "24.05";
+            wsl.enable = true;
+          }
+	  ./machines/wsl/configuration.nix
+        ];
       };
     };
+  };
 }
