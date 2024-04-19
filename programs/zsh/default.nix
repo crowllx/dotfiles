93df9d7623602aa8d84988fdffb9bf5396cabc78
wshine <114693487+WinstonShine@users.noreply.github.com>
2024-04-19 03:01:38 +0000

    restructure

--- programs/zsh/default.nix
+++ programs/zsh/default.nix
@@ -1,3 +1,22 @@
 { pkgs, lib, config, inputs }: {
-    options = modules.zsh.enable = lib.mkEnableOption "enables zsh"
+    options ={ modules.zsh.enable = lib.mkEnableOption "enables zsh"; };
+    config = lib.mkIf config.modules.zsh.enable {
+        programs.zsh = {
+            enable = true;
+            enableCompletion = true;
+            autosuggestion.enable = true;
+            autosuggestion.highlight = true;
+            dotDir = ".config/zsh";
+            oh-my-zsh = {
+                enable = true;
+                plugins = [ "git" "git-prompt" "git-extras" "aliases" ]; 
+            };
+            syntaxHighlighting.enable = true;
+            initExtra = ''
+                ${builtins.readFile ./zsh-theme }
+                ${builtins.readFile ./.zshrc }
+            '';
+        };
+        
+    };
 }
