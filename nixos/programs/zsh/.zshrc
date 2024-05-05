48e657da388deb1ab2dde9abfae84ffe647885e0
crowllx <114693487+crowllx@users.noreply.github.com>
2024-05-04 20:49:40 -0700

    Revert "add nixos config"
    This reverts commit 7ed5b8616baf560ddb380cc23b073431dc1a8cab.

--- nixos/programs/zsh/.zshrc
+++
@@ -1,7 +0,0 @@
-# source <(gopass completion bash)
-alias tmux=tmux -2
-
-function rebuild {
-    echo "$1 $2"
-    sudo nixos-rebuild $1 --flake "${HOME}/tools/dotfiles/nixos/#${2}"
-}
---
+++ nvim/init.lua
@@ -0,0 +1,35 @@
+require("wshine.set")
+require("wshine.remap")
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+vim.g.term = "screen-256color"
+vim.g.netrw_browse_split = 0
+vim.g.netrw_hide = 1
+vim.g.netrw_winsize = 25
+vim.g.netrw_browsex_viewer = "cmd.exe /C start"
+vim.g.netrw_browsex_support_remote = true
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+-- vimwiki
+vim.g.vimwiki_listsyms = '✗○◐●✓'
+vim.g.vimwiki_list = { { path = '~/notes/', syntax = 'markdown', ext = '.md' } }
+
+-- lazy nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("wshine.lazy")
+
+
