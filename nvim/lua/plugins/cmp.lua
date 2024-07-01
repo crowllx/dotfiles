02c1d8396ed37dd4001d594c95cf08cefcdf6e96
crowllx <114693487+crowllx@users.noreply.github.com>
2024-07-01 13:57:57 -0700

    update cmp plugin

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -13,7 +13,11 @@ return {
         local cmp = require('cmp')
         local cmp_action = lsp_zero.cmp_action()
         local cmp_select_opts = { behavior = cmp.SelectBehavior.Select }
-
+        ls = require("luasnip")
+        require("luasnip.loaders.from_vscode").lazy_load()
+        vim.keymap.set({ "i" }, "<C-K>", function() ls.expand() end, { silent = true })
+        vim.keymap.set({ "i", "s" }, "<C-L>", function() ls.jump(1) end, { silent = true })
+        vim.keymap.set({ "i", "s" }, "<C-H>", function() ls.jump(-1) end, { silent = true })
         cmp.setup({
             formatting = lsp_zero.cmp_format({ details = true }),
             sources = {
